3986c140e8f29c57250e3c0aca2eadb82505223b
Sheng Yunzhou <shengyunzhou@gmail.com>
2018-06-24 15:25:49 +0800

    Add heroku deployment

--- bin/build_frontend.sh
+++ bin/build_frontend.sh
@@ -5,6 +5,12 @@ cd src/main/resources/webapp
 npm install
 npm t
 npm run build
+if [ ! -d "../static" ]; then
+    cd ..
+    mkdir static
+    cd webapp
+fi
+
 ls -ltra ../static
 rm -rf ../static/**
 mv build/* ../static/
